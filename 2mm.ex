f31d1ad54a58cdd34c48bc015ceb4f5c06e06d35
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2025-05-26 12:14:24 -0300

    initial commit

---
+++ 2mm.ex
@@ -0,0 +1,45 @@
+require PolyHok
+
+PolyHok.defmodule MM2 do
+    @ni 1024 # probably needs to be changable later
+    @nj 1024
+    @nl 1024
+    @nk 1024
+
+def init_array(ni, nj, nk, nl, A, B, C, D) do
+    alpha = 32412
+    beta = 2123
+
+    A = 
+        Enum.map(matrix, fn row ->
+            Enum.map(row, fn val -> val * 2 end)
+        end)
+
+    {alpha, beta}
+end
+
+def compare_results() do
+
+end
+
+def gpu_argv_init() do
+
+end
+
+defk mm2_kernel1() do
+
+end
+
+defk mm2_kernel2() do
+
+end
+
+def print_array() do
+
+end
+
+def mm2_polyhok() do
+
+end
+
+end
