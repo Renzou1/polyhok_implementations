03b3a64c7c1e25a5944e5cd71284ad79dbeb0a01
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2025-06-05 09:57:25 -0300

    removed module attributes

--- 2mm.ex
+++ 2mm.ex
@@ -1,10 +1,6 @@
 require PolyHok
 
 PolyHok.defmodule MM2 do
-    @ni 1024 # probably needs to be changable later
-    @nj 1024
-    @nl 1024
-    @nk 1024
 
 def init_array(ni, nj, nk, nl, a, b, c, d) do
     alpha = 32412
@@ -14,28 +10,28 @@ def init_array(ni, nj, nk, nl, a, b, c, d) do
     a =
         for i <- 0..ni-1 do
             for j <- 0..nk-1 do
-                (i * j) / @ni
+                (i * j) / ni
             end
         end
 
     b = 
         for i <- 0..nk-1 do
             for j <- 0..nj-1 do
-                (i * (j + 1))  / @nj
+                (i * (j + 1))  / nj
             end
         end
 
     c = 
         for i <- 0..nl-1 do
             for j <- 0..nj-1 do
-                (i * (j + 3)) / @nl
+                (i * (j + 3)) / nl
             end
         end
 
     d = 
         for i <- 0..ni-1 do
             for j <- 0..nl-1 do
-                (i * (j + 2)) / @nk
+                (i * (j + 2)) / nk
             end
         end
 
@@ -55,16 +51,16 @@ defk mm2_kernel1(ni, nj, nk, nl, alpha, beta, tmp, a, b) do
 	j = blockIdx.x * blockDim.x + threadIdx.x
 	i = blockIdx.y * blockDim.y + threadIdx.y
 
-    if (i < @ni && j < @nj) do
-        tmp[i * @nj + j] = map_kernel1_helper(alpha, a, b, j, i)
+    if (i < ni && j < nj) do
+        tmp[i * nj + j] = map_kernel1_helper(nj, nk, alpha, a, b, j, i)
     end
 
 end
 
-defd map_kernel1_helper(alpha, a, b, j, i) do 
+defd map_kernel1_helper(nj, nk, alpha, a, b, j, i) do 
     acc = 0
-    for k <- 0..@nk-1 do
-        acc = acc + alpha * a[i * @nk + k] * b[k * @nj + j]
+    for k <- 0..nk-1 do
+        acc = acc + alpha * a[i * nk + k] * b[k * nj + j]
     end
 
     return acc
@@ -74,16 +70,16 @@ defk mm2_kernel2(ni, nj, nk, nl, alpha, beta, tmp, c, d) do
     j = blockIdx.x * blockDim.x + threadIdx.x
 	i = blockIdx.y * blockDim.y + threadIdx.y
 
-	if ((i < @ni) && (j < @nl)) do 
-        D[i * @nl + j] = mm2_kernel2_helper(D[i * @nl + j], beta, tmp, c,  j, i)
+	if ((i < ni) && (j < nl)) do 
+        D[i * nl + j] = mm2_kernel2_helper(nj, nl, D[i * nl + j], beta, tmp, c,  j, i)
 	end
 end
 
-defd mm2_kernel2_helper(acc, beta, tmp, c,  j, i) do
+defd mm2_kernel2_helper(nj, nl, acc, beta, tmp, c,  j, i) do
     acc = acc * beta
 
-    for k <- 0..@nj-1 do
-        acc =  acc + tmp[i * @nj + k] * c[k * @nl + j]
+    for k <- 0..nj-1 do
+        acc =  acc + tmp[i * nj + k] * c[k * nl + j]
     end
 
     return acc
