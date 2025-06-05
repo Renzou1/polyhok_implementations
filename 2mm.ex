092ae500addde88466971b128eb42c3231665f41
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2025-06-05 10:57:29 -0300

    rough mm2_polyhok function implementation

--- 2mm.ex
+++ 2mm.ex
@@ -2,7 +2,7 @@ require PolyHok
 
 PolyHok.defmodule MM2 do
 
-def init_array(ni, nj, nk, nl, a, b, c, d) do
+def init_array(ni, nj, nk, nl) do
     alpha = 32412
     beta = 2123
 
@@ -36,7 +36,7 @@ def init_array(ni, nj, nk, nl, a, b, c, d) do
         end
 
 
-    {alpha, beta}
+    {alpha, beta, a, b, c, d}
 end
 
 def compare_results(ni, nl, d, d_outputFromGpu) do
@@ -89,8 +89,34 @@ def print_array(ni, nl, d) do
 
 end
 
-def mm2_polyhok(ni, nj, nk, nl, alpha, beta, temp, a, b, c, d, d_outputFromGpu) do
+def mm2_polyhok(ni, nj, nk, nl, alpha, beta, tmp, a, b, c, d) do    
 
+tmp_gpu = PolyHok.new_gnx(tmp, type: f32) #hardcoded types for now
+a_gpu = PolyHok.new_gnx(a, type: f32)
+b_gpu = PolyHok.new_gnx(b, type: f32)
+c_gpu = PolyHok.new_gnx(c, type: f32)
+d_gpu = PolyHok.new_gnx(d, type: f32)
+
+block = {32, 8, 1}
+x = 0
+y = 1
+z = 2
+grid1 = {Float.ceil(nj / elem(block, x)), Float.ceil(ni / elem(block, y)), 1}
+grid2 = {Float.ceil(nl / elem(block, x)), Float.ceil(ni / elem(block,y)), 1}
+
+PolyHok.spawn(&MM2.mm2_kernel1/9, grid1, block, [ni, nj, nk, nl, alpha, beta, tmp_gpu, a_gpu, b_gpu])
+PolyHok.spawn(&MM2.mm2_kernel2/9, grid2, block, [ni, nj, nk, nl, alpha, beta, tmp_gpu, c_gpu, d_gpu])
+
+d_outputFromGpu = PolyHok.get_gnx(d_gpu)
+d_outputFromGpu
+end
 end
 
-end
+ni = 1024
+nj = 1024
+nk = 1024
+nl = 1024
+
+{alpha, beta, a, b, c, d} = init_array(ni, nj, nk, nl)
+
+mm2_polyhok(ni, nj, nk, nl, alpha, beta, tmp, a, b, c, d, d_outputFromGpu)
