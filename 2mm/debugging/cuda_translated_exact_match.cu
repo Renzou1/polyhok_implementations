8e07042e7187fb17fcd92e6c03bb00ecc571b9b1
Renzo <renzo.battisti1@gmail.com>
2025-07-02 21:37:57 -0300

    updated the cuda translation of the exact match variant

--- 2mm/debugging/cuda_translated_exact_match.cu
+++ 2mm/debugging/cuda_translated_exact_match.cu
@@ -1,13 +1,13 @@
 
-extern "C" __global__ void mm2_kernel(int ni, int nj, int nk, int nl, int alpha, int beta, float *tmp, float *a, float *b)
+extern "C" __global__ void mm2_kernel(int ni, int nj, int nk, int nl, float alpha, float beta, float *tmp, float *a, float *b)
 {
 	int j = ((blockIdx.x * blockDim.x) + threadIdx.x);
 	int i = ((blockIdx.y * blockDim.y) + threadIdx.y);
-if(((i < ni) && (j < nj)))
+if(((i < 1024) && (j < 1024)))
 {
-	tmp[((i * nj) + j)] = 0.0;
+	tmp[((i * 1024) + j)] = 0.0;
 for( int k = 0; k<nk; k+=1){
-	tmp[((i * nj) + j)] = (tmp[((i * nj) + j)] + ((alpha * a[((i * nk) + k)]) * b[((k * nj) + j)]));
+	tmp[((i * 1024) + j)] = (tmp[((i * 1024) + j)] + ((alpha * a[((i * 1024) + k)]) * b[((k * 1024) + j)]));
 }
 
 }
@@ -16,17 +16,18 @@ for( int k = 0; k<nk; k+=1){
 
 
 
-extern "C" __global__ void mm2_kernel2(int ni, int nj, int nk, int nl, int alpha, int beta, float *tmp, float *c, float *d)
+extern "C" __global__ void mm2_kernel2(int ni, int nj, int nk, int nl, float alpha, float beta, float *tmp, float *c, float *d)
 {
 	int j = ((blockIdx.x * blockDim.x) + threadIdx.x);
 	int i = ((blockIdx.y * blockDim.y) + threadIdx.y);
-if(((i < ni) && (j < nl)))
+if(((i < 1024) && (j < 1024)))
 {
-	d[((i * nl) + j)] = (d[((i * nl) + j)] * beta);
-for( int k = 0; k<nj; k+=1){
-	d[((i * nl) + j)] = (d[((i * nl) + j)] + (tmp[((i * nj) + k)] * c[((k * nl) + j)]));
+	d[((i * 1024) + j)] = (d[((i * 1024) + j)] * beta);
+for( int k = 0; k<1024; k+=1){
+	d[((i * 1024) + j)] = (d[((i * 1024) + j)] + (tmp[((i * 1024) + k)] * c[((k * 1024) + j)]));
 }
 
 }
 
 }
+
