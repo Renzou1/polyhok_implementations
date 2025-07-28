91664939444f03317f6a27128ffd2c93763413a2
Renzo <renzo.battisti1@gmail.com>
2025-07-27 23:53:54 -0300

    general fixes

--- correlation/correlation.ex
+++ correlation/correlation.ex
@@ -23,12 +23,12 @@ defk mean_kernel(m, n, mean, data, float_n, f) do
 	j = blockIdx.x * blockDim.x _ threadIdx.x
 
     if (j < m) do
-      mean[j] = f(mean[j], data, float_n, j)
+      mean[j] = f(n, data, float_n, j)
     end
 
 end
 
-defd mean_kernel_helper(mean_j, data, float_n, j) do
+defd mean_kernel_helper(m, n, data, float_n, j) do
   mean_j = 0.0
 
   for i in range(0, n, 1) do
@@ -44,15 +44,15 @@ defk std_kernel(m, n, mean, std, data, float_n, eps, f) do
   j = blockIdx.x * blockDim.x + threadIdx.x
 
   if (j < m) do
-    std[j] = f(std[j], data, mean, float_n, eps)
+    std[j] = f(n, data, mean, float_n, eps)
   end
 end
 
-defd std_kernel_helper(std_j, data, mean, float_n, eps) do
+defd std_kernel_helper(m, n, data, mean, float_n, eps) do
 		std_j = 0.0
 
     for i in range(0, n, 1) do
-			std_j = std_j + (data[i*M + j] - mean[j]) * (data[i*M + j] - mean[j])
+			std_j = std_j + (data[i*m + j] - mean[j]) * (data[i*m + j] - mean[j])
     end
 
 		std_j = std_j / float_n
@@ -76,7 +76,7 @@ end
 
 defd reduce_kernel_helper(data_im_j, mean_j, std_j, float_n) do
   data_im_j = data_im_j - mean_j
-  data_im_j = data_im_j / (math.sqrt(float_n) * std_j)
+  #data_im_j = data_im_j / (Nx.sqrt(float_n) * std_j)
   return data_im_j
 end
 
@@ -86,26 +86,26 @@ defk corr_kernel(m, n, symmat, data) do
 
   if (j1 < m - 1) do
 
-		symmat[j1*M + j1] = 1.0
+		symmat[j1*m + j1] = 1.0
 
     for j2 in range(j1 + 1, m, 1) do
 
-      symmat[j1*M + j2] = 0.0
+      symmat[j1*m + j2] = 0.0
 
       for i in range(0, n, 1) do
 
-				symmat[j1*M + j2] = symmat[j1*M + j2] + data[i*M + j1] * data[i*M + j2]
+				symmat[j1*m + j2] = symmat[j1*m + j2] + data[i*m + j1] * data[i*m + j2]
 
       end
 
-			symmat[j2*M + j1] = symmat[j1*M + j2]
+			symmat[j2*m + j1] = symmat[j1*m + j2]
 
     end
 
   end
 end
 
-def correlation_polyhok(m, n, data, mean, stddev, symmat, symmat_gpu_output, float_n, eps) do
+def correlation_polyhok(m, n, data, mean, stddev, symmat, float_n, eps) do
   data_gpu = PolyHok.new_gnx(data)
   symmat_gpu = PolyHok.new_gnx(symmat)
   stddev_gpu = PolyHok.new_gnx(stddev)
@@ -118,27 +118,22 @@ def correlation_polyhok(m, n, data, mean, stddev, symmat, symmat_gpu_output, flo
   grid2 = { ceil(m / 256), 1, 1}
 
   block3 = {32, 8, 1}
-  grid3 = { ceil(m / 32), n / 8, 1}
+  grid3 = { ceil(m / 32), ceil(n / 8), 1}
 
   block4 = {256, 1, 1}
   grid4 = { ceil(m / 256), 1, 1}
 
-  #defk mean_kernel(m, n, mean, data, float_n, f) do, defd mean_kernel_helper(mean_j, data, float_n, j) do
-  #defk std_kernel(m, n, mean, std, data, float_n, eps, f) do, defd std_kernel_helper(std_j, data, mean, float_n, eps) do
-  #defk reduce_kernel(m, n, mean, std, data, float_n, f) do, defd reduce_kernel_helper(data_im_j, mean_j, std_j, float_n) do
-  #defk corr_kernel(m, n, symmat, data) do
-
   prev = System.monotonic_time()
 
-  PolyHok.spawn(&CORR.mean_kernel/5, grid1, block1, [m, n, mean_gpu, data_gpu, float_n, &CORR.mean_kernel_helper/4])
-  PolyHok.spawn(&CORR.std_kernel/8, grid2, block2, [m, n, mean_gpu, stddev_gpu, data_gpu, float_n, eps, &CORR.std_kernel_helper/5])
+  PolyHok.spawn(&CORR.mean_kernel/5, grid1, block1, [m, n, mean_gpu, data_gpu, float_n, &CORR.mean_kernel_helper/5])
+  PolyHok.spawn(&CORR.std_kernel/8, grid2, block2, [m, n, mean_gpu, stddev_gpu, data_gpu, float_n, eps, &CORR.std_kernel_helper/6])
   PolyHok.spawn(&CORR.reduce_kernel/7, grid3, block3, [m, n, mean_gpu, stddev_gpu, data_gpu, float_n, &CORR.reduce_kernel_helper/4])
-  PolyHok.spawn(&CORR.corr_kernel/4, grid3, block3, [m, n, symmat_gpu, data_gpu])
+  PolyHok.spawn(&CORR.corr_kernel/4, grid4, block4, [m, n, symmat_gpu, data_gpu])
 
   next = System.monotonic_time()
 
   symmat_gpu_output = PolyHok.get_gnx(symmat_gpu)
-  symmat_gpu_output[(m-1)*m + (m-1)] = 1.0 # i dont know why this is done
+  Nx.indexed_put(symmat_gpu_output, Nx.tensor([[m-1, m-1]]), Nx.tensor([1.0])) # i dont know why this is done
 
   #CORR.write_tensor_to_file(symmat_gpu_output, "polyhok_output.txt")
   IO.puts "PolyHok\t#{inspect(symmat_gpu_output)}\t#{System.convert_time_unit(next-prev,:native,:millisecond)} "
@@ -157,10 +152,13 @@ end
 
 float_n = 3214212.01
 eps = 0.005
-data = CORR.init_arrays(10, 10)
+type = {:f, 32}
+m = 10
+n = 10
 
-data = Nx.broadcast(Nx.tensor(0, type: type), {m, n})
+data = CORR.init_arrays(m, n)
 mean = Nx.broadcast(Nx.tensor(0, type: type), {m, n})
 stddev = Nx.broadcast(Nx.tensor(0, type: type), {m, n})
 symmat = Nx.broadcast(Nx.tensor(0, type: type), {m, n})
-symmat_gpu_output = Nx.broadcast(Nx.tensor(0, type: type), {m, n})
+
+CORR.correlation_polyhok(m, n, data, mean, stddev, symmat, float_n, eps)
